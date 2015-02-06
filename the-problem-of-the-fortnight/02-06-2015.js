3b72c04625e00e39c6da1dfc9ba75070e4656162
Joshua L Geschwendt <geschwendt@users.noreply.github.com>
2015-02-06 14:30:34 -0500

    Update 02-06-2015.js

--- the-problem-of-the-fortnight/02-06-2015.js
+++ the-problem-of-the-fortnight/02-06-2015.js
@@ -9,27 +9,27 @@
         for (var d = 3 ; d < 9 ; d++ ){
           for (var e = 3 ; e < 9 ; e++ ){
             if (
-              a != b &&
-              a != c &&
-              a != d &&
-              a != e &&
-              b != c &&
-              b != d &&
-              b != e &&
-              c != d &&
-              c != e &&
-              d != e &&
-              (a + b + c) == 19 &&
-              (a + d + e) == 19
+              a !== b &&
+              a !== c &&
+              a !== d &&
+              a !== e &&
+              b !== c &&
+              b !== d &&
+              b !== e &&
+              c !== d &&
+              c !== e &&
+              d !== e &&
+              (a + b + c) === 19 &&
+              (a + d + e) === 19
             ) {
               console.log('# -----------------------');
               console.log('# Solution ' + solution   );
               console.log('# -----------------------');
-              console.log(' A: ' + a);
-              console.log(' B: ' + b);
-              console.log(' C: ' + c);
-              console.log(' D: ' + d);
-              console.log(' E: ' + e);
+              console.log('\tA: ' + a);
+              console.log('\tB: ' + b);
+              console.log('\tC: ' + c);
+              console.log('\tD: ' + d);
+              console.log('\tE: ' + e);
               console.log('\n\n\n')
             }
           }
